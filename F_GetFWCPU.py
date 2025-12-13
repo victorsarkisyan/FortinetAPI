af042b93d9f4dd42eedb6859b187988d3e2d008f
victorsarkisyan <54569499+victorsarkisyan@users.noreply.github.com>
2025-12-12 18:52:01 -0800

    Changes

---
+++ F_GetFWCPU.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+import sys
+import urllib3
+
+# Disable insecure request warnings
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+ 
+api_key = sys.argv[1]
+controller_ip = sys.argv[2]
+
+url = f"https://{controller_ip}/api/v2/monitor/system/interface"
+
+params = {
+}
+
+headers = {
+  'Content-Type': 'application/json',
+  'Authorization': f'Bearer {api_key}'   
+}
+
+response = requests.get(url, headers=headers, params=params,  verify=False)
+
+output = response.json()
+
+results = output.get("results", [])
+print(results)
