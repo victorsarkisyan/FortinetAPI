eb8347b488573adfcca8d9dad30ab65d974e81f1
victorsarkisyan <54569499+victorsarkisyan@users.noreply.github.com>
2025-12-14 12:16:46 -0800

    Create F_GetFWModel.py

---
+++ F_GetFWModel.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+import sys
+import urllib3
+
+# Disable insecure request warnings
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+ 
+api_key = sys.argv[1]
+controller_ip = sys.argv[2]
+
+url = f"https://{controller_ip}/api/v2/monitor/system/status"
+
+params = {
+}
+
+headers = {
+  'Content-Type': 'application/json',
+  'Authorization': f'Bearer {api_key}'   
+}
+
+response = requests.get(url, headers=headers, params=params,  verify=False)
+
+output = response.json()
+
+print(output['results']['model'])
